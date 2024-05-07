02dfaa2620232f9460f0b772f88fae09bddad0b2
CristianN01 <cristiannechifor71@gmail.com>
2024-05-07 17:02:23 +0200

    aggiunto file index e script e scritto le variabili su js con il finale che appare sul documento html

---
+++ script.js
@@ -0,0 +1,9 @@
+let myName = prompt("inserisci il tuo nome");
+
+let mySurname = prompt("inserisci il tuo cognome");
+
+let myColor= prompt("inserisci il tuo colore preferito");
+
+const myNumber = (24)
+
+document.getElementById("text").innerHTML = (myName + mySurname + myColor + myNumber);
